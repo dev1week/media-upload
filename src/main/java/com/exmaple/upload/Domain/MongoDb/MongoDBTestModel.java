99dad21d22306483fc73de7cdbe18f530ede4ccf
dev1week <rondo2860@gmail.com>
2023-03-26 23:09:36 +0900

    #9 -1 도메인 작성

---
+++ src/main/java/com/exmaple/upload/Domain/MongoDb/MongoDBTestModel.java
@@ -0,0 +1,16 @@
+package com.exmaple.upload.Domain.MongoDb;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "user")
+public class MongoDBTestModel {
+
+    private String id;
+    private String name;
+    private int age;
+}
